28862b33f7836699d09fd6e420dd9c30d758cbf2
Porimol Chandro <i-porimal.chandro@ekkdam.com>
2018-04-06 22:19:55 +0600

    setup.py updated

--- setup.py
+++ setup.py
@@ -8,18 +8,14 @@ with open('README.md', 'r') as f:
 
 setup(
     name='bnbphoneticparser',
-    version='0.1.4',
+    version='0.1.5',
     python_requires='>3.0.0',
-    install_requires = ['bnbphoneticparser'],
-    package_dir = {
-        '': 'bnbphoneticparser'
-    },
-    packages = find_packages('bnbphoneticparser', exclude='tests'),
+    packages = find_packages(exclude='tests'),
     url='https://github.com/porimol/bnbphoneticparser',
     license='MIT License',
     author='Porimol Chandro',
     author_email='porimolchandroroy@gmail.com',
     description='Bengali Phonetic Parser',
     long_description=long_description,
-    keywords = ['NLP', 'Python', 'Bengali to Banglish', 'Banglish to Bengali'],
+    keywords = ['NLP', 'Python', 'Bengali', 'Banglish'],
 )
