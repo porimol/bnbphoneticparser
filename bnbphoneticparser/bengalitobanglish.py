fe7f896e3b3a361ff195bfdbfaf3a8592477f6a6
dibosh <dibosh@newscred.com>
2018-04-07 15:17:15 +0600

    Refactory, simplify & DRY up bengalitoenglish

--- bnbphoneticparser/bengalitobanglish.py
+++ bnbphoneticparser/bengalitobanglish.py
@@ -20,120 +20,96 @@ class BengaliToBanglish(BengaliPhoneticParser):
         self.__byanjon_borno = dict((v.strip(), k.strip()) for k, v in self.byanjon_borno.items())
         self.__jukto_borno = dict((v.strip(), k.strip()) for k, v in self.jukto_borno.items())
 
-
-    def __change(self, txt, ch, nch):
-        return txt.replace(ch, nch)
-
-
     def __change_sworborno(self, txt, ch):
         sx = ""
         sx += txt
-        if (ch == "আ"):
+        if ch == "আ":
             asx = ""
-            for i in range(0, len(txt)):
-                if (i == 0):
-                    if ("" + txt[i] == "আ"):
+            for i, _ch in enumerate(txt):
+                if i == 0:
+                    if _ch == "আ":
                         asx += "a"
                     else:
-                        asx += txt[i]
+                        asx += _ch
                 else:
-                    if (("" + txt[i] == "আ") and (("" + txt[i - 1] in self.__shoroborno.values()) or
-                                                              ("" + txt[i - 1] in self.__shoroborno.keys()) or
-                                                              ("" + txt[i - 1] in self.__kar.keys()) or
-                                                              ("" + txt[i - 1] in self.__kar.values()))):
-                        if (("" + txt[i - 1] == "a") or ("" + txt[i - 1] == "a") or ("" + txt[i - 1] == "আ") or (
-                                        "" + txt[i - 1] == "অ")):
+                    _prev_ch = txt[i - 1]
+                    if _ch == "আ" and self._is_sworborno(_prev_ch) or self._is_kar(_prev_ch):
+                        if _prev_ch == "a" or _prev_ch == "আ" or _prev_ch == "অ":
                             asx += "a"
                         else:
                             asx += "ya"
-                    elif (("" + txt[i] == "অ") and (("" + txt[i - 1] in self.__byanjon_borno.values()) or
-                                                                ("" + txt[i - 1] in self.__byanjon_borno.keys()) or (
-                                    "" + txt[i - 1] in self.__byanjon_borno.keys())
-                                                            or ("" + txt[i - 1] in self.__byanjon_borno.values()))):
+                    elif _ch == "অ" and self._is_byanjonborno(_prev_ch):
                         asx += "আ"
-                    elif txt[i] in self.__kar.keys():
-                        asx += "" + self.__kar[txt[i]]
+                    elif self._is_kar(_ch):
+                        asx += self.__kar[_ch]
                     else:
-                        asx += "" + txt[i]
+                        asx += _ch
+
             return asx
         else:
             ofe = sx.find(ch, 0)
             ofs = 0
             while ofs < len(txt) and (ofe != -1):
                 ofe = sx.find(ch, ofs)
-                # print(ofe)
-                if (ofe == -1):
+                if ofe == -1:
                     break
                 else:
-                    if (ofe == 0):
-                        # print(sx)
+                    if ofe == 0:
                         sx = sx.replace(sx[ofe:ofe + len(ch)], self.__shoroborno[ch])
                     else:
-                        if (ch == "ও" and (("" + txt[ofe - 1] in self.__shoroborno.values()) or (
-                                        "" + txt[ofe - 1] in self.__shoroborno.keys())
-                                           or ("" + txt[ofe - 1] in self.__kar.keys()) or (
-                                        "" + txt[ofe - 1] in self.__kar.values()))):
+                        _prev_och = txt[ofe - 1]
+                        if ch == "ও" and self._is_sworborno(_prev_och) or self._is_kar(_prev_och):
                             sx = sx.replace(sx[ofe:ofe + 1], "ও")
-                        elif (("" + txt[ofe - 1] in self.__shoroborno.values()) or (
-                                        "" + txt[ofe - 1] in self.__shoroborno.keys())
-                              or ("" + txt[ofe - 1] in self.__kar.keys()) or (
-                                        "" + txt[ofe - 1] in self.__kar.values()) or
-                                  ("" + txt[ofe - 1] == "ও")):
+                        elif self._is_sworborno(_prev_och) or self._is_kar(_prev_och) or _prev_och == "ও":
                             sx = sx.replace(sx[ofe:ofe + len(ch)], self.__shoroborno[ch])
                         else:
-                            if ("" + txt[ofe] != "ও"):
+                            if txt[ofe] != "ও":
                                 sx = sx.replace(sx[ofe:ofe + len(ch)], self.__kar[ch])
                 ofs = ofe + 1
 
         return sx
 
-
     def __change_x(self, txt):
         sx = ""
-        if ("" + txt == "এক্স"):
+        if txt == "এক্স":
             sx += "x"
-        elif ("" + txt == "ক্স"):
+        elif txt == "ক্স":
             sx += "x"
         else:
             sx += txt
 
         return sx
 
-
     def __is_alphabet(self, code):
         return code.isalpha()
-
 
     def __convert(self, bengali_string):
 
         bengali_string = self.__change_x(bengali_string)
         for a_five_char in self.fivechar:
-            bengali_string = self.__change(bengali_string, a_five_char, self.__jukto_borno[a_five_char])
+            bengali_string = self._change_to(bengali_string, a_five_char, self.__jukto_borno[a_five_char])
 
         for a_four_char in self.fourchar:
-            bengali_string = self.__change(bengali_string, a_four_char, self.__jukto_borno[a_four_char])
+            bengali_string = self._change_to(bengali_string, a_four_char, self.__jukto_borno[a_four_char])
 
         for a_three_char in self.threechar:
-            bengali_string = self.__change(bengali_string, a_three_char, self.__jukto_borno[a_three_char])
+            bengali_string = self._change_to(bengali_string, a_three_char, self.__jukto_borno[a_three_char])
         bengali_string = self.__change_sworborno(bengali_string, "rri")
 
         for a_two_char in self.twochar:
-            if (a_two_char in self.__shoroborno.keys()):
+            if self._is_sworborno(a_two_char):
                 bengali_string = self.__change_sworborno(bengali_string, a_two_char)
-            elif (a_two_char in self.__byanjon_borno.keys()):
-                bengali_string = self.__change(bengali_string, a_two_char, self.__byanjon_borno[a_two_char])
-            elif (a_two_char in self.__byanjon_borno.keys()):
-                bengali_string = self.__change(bengali_string, a_two_char, self.__byanjon_borno[a_two_char])
+            elif self._is_byanjonborno(a_two_char):
+                bengali_string = self._change_to(bengali_string, a_two_char, self.__byanjon_borno[a_two_char])
 
         for a_one_char in self.onechar:
-            if (a_one_char in self.__shoroborno.keys()):
+            if self._is_sworborno(a_one_char):
                 bengali_string = self.__change_sworborno(bengali_string, a_one_char)
-            elif (a_one_char in self.__byanjon_borno.keys()):
-                bengali_string = self.__change(bengali_string, a_one_char, self.__byanjon_borno[a_one_char])
-        bengali_string = self.__change(bengali_string, "ও", "")
+            elif self._is_byanjonborno(a_one_char):
+                bengali_string = self._change_to(bengali_string, a_one_char, self.__byanjon_borno[a_one_char])
+        bengali_string = self._change_to(bengali_string, "ও", "")
 
         return bengali_string
-
 
     def parse(self, bengali_text_to_parse):
         separated_word = bengali_text_to_parse.split(" ")
